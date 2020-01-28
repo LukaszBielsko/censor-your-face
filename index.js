4dbd532f773ed2acd72bcbe5124c444f9792502f
Lukasz Walczak <lukeabove@gmail.com>
2020-01-28 11:53:22 +0100

    face detection added

--- index.js
+++ index.js
@@ -6,4 +6,31 @@ const ctx = canvas.getContext('2d')
 const face = document.querySelector('.face')
 const faceCtx = canvas.getContext('2d')
 
+const faceDetector = new FaceDetector()
 
+
+const displayVideo = async () => {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: { width: 1000, height: 600}
+  })
+
+  // assign video source 
+  video.srcObject = stream
+  await video.play()
+
+  // size canvas and face canvas the same as video 
+  canvas.height = video.videoHeight
+  canvas.width = video.videoWidth
+  face.height = video.videoHeight
+  face.width = video.videoWidth
+}
+
+
+const faceDetect = async () => {
+  const faces = await faceDetector.detect(video)
+  console.log(faces.length)
+  // ask browser for next animation frame, then fire off detect
+  requestAnimationFrame(faceDetect)
+}
+
+displayVideo().then(faceDetect)
