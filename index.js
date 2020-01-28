256a09a1f86009a45549d237f0e9aaa073d6ec31
Lukasz Walczak <lukeabove@gmail.com>
2020-01-28 11:16:21 +0100

    base files added

---
+++ index.js
@@ -0,0 +1,9 @@
+const video = document.querySelector('.webcam')
+
+const canvas = document.querySelector('.video')
+const ctx = canvas.getContext('2d')
+
+const face = document.querySelector('.face')
+const faceCtx = canvas.getContext('2d')
+
+
